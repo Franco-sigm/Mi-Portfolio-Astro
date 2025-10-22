672603e9265ace596cd2a6b3a9f12f1c63f5710c
Franco-sigm <francoherrera.30@outlook.com>
2025-10-22 17:42:28 -0300

    github page configurations astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Franco-sigm.github.io', 
+  base: '/Mi-Portfolio-Astro/',
   // 2. Dile a Astro que la use
   integrations: [tailwind()]
 });
